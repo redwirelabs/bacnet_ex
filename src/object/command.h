4f4a74af5c9f3d797fc01859652061d977fa5767
abelino <abelino.romo@gmail.com>
2025-02-06 21:43:43 -0800

    Add routable Command Object impl

--- src/object/command.h
+++ src/object/command.h
@@ -1,6 +1,27 @@
 #ifndef BACNET_OBJECT_COMMAND_H
 #define BACNET_OBJECT_COMMAND_H
 
+#include <bacnet/bacaction.h>
+
+#ifndef MAX_COMMAND_ACTIONS
+#define MAX_COMMAND_ACTIONS 8
+#endif
+
+#define MAX_OBJ_NAME_LEN 32
+#define MAX_OBJ_DESC_LEN 64
+
+typedef struct {
+  BACNET_OBJECT_TYPE type;
+
+  uint32_t present_value;
+  bool     in_progress;
+  bool     successful;
+  char     name[MAX_OBJ_NAME_LEN];
+  char     description[MAX_OBJ_DESC_LEN];
+
+  BACNET_ACTION_LIST actions[MAX_COMMAND_ACTIONS]; // TODO: make this dynamic?
+} COMMAND_OBJECT;
+
 void command_init(void);
 unsigned command_count(void);
 uint32_t command_index_to_instance(unsigned index);
